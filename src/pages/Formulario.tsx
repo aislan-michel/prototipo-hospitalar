a945ecb08ea019141c53614713934a0b653d1853
Aislan Michel Moreira Freitas <aislan.michel92@gmail.com>
2022-09-05 21:36:13 -0300

    adicionando campo e calculo de idade

--- src/pages/Formulario.tsx
+++ src/pages/Formulario.tsx
@@ -1,19 +1,27 @@
 import { StyleSheet, Text, View, Image, TouchableOpacity, TextInput } from 'react-native';
-import {useNavigation} from '@react-navigation/native';
-import {StackNavigationProp} from '@react-navigation/stack';
+import { useNavigation } from '@react-navigation/native';
+import { StackNavigationProp } from '@react-navigation/stack';
 import { RootStackParamList } from '../types/RootStackPrams';
+import {useState} from 'react';
+import moment from 'moment';
 
 import globalStyles from '../styles/global';
-
-import {Button} from '../components/Button'
+import { Button } from '../components/Button'
 
 type formularioProp = StackNavigationProp<RootStackParamList, 'Formulario'>;
 
 export default function Formulario() {
+  const [idade, setIdade] = useState('');
+
   const navigation = useNavigation<formularioProp>();
 
   function conectar(): void {
     navigation.navigate('Camas');
+  }
+
+  function calcularIdade(dataNascimento: string): void{
+    const idade = moment(new Date()).diff(dataNascimento, 'years');
+    setIdade(idade.toString());
   }
 
   return (
@@ -23,7 +31,10 @@ export default function Formulario() {
 
             <TextInput style={styles.input} placeholder='Nome do paciente' />
 
-            <TextInput keyboardType='numbers-and-punctuation' style={styles.input} placeholder='Data de nascimento' />
+            <TextInput keyboardType='numbers-and-punctuation' style={[styles.input]} placeholder='Data de nascimento (yyyy-mm-dd)' 
+                  onChangeText={value => calcularIdade(value)} />
+
+            <TextInput style={styles.input} placeholder='Idade' editable={false} value={idade} />
 
             <TextInput style={[styles.input, { marginTop: 40 }]} placeholder='Nome do acompanhante' />
 
