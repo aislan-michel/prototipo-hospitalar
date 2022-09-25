65ce325780115266aa7ff914b4cda7a8738b58b8
Aislan Michel Moreira Freitas <aislan.michel92@gmail.com>
2022-09-25 16:31:15 -0300

    finalizando, parcialmente, formulario

--- src/pages/Formulario.tsx
+++ src/pages/Formulario.tsx
@@ -1,20 +1,18 @@
-import { StyleSheet, Text, View, TextInput, Platform, Button } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Platform } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { StackNavigationProp } from '@react-navigation/stack';
 import { RootStackParamList } from '../types/RootStackPrams';
 import React, {useState} from 'react';
 import moment from 'moment';
-import DatePicker from 'react-native-date-picker';
 
 import { RoundedButton } from '../components/RoundedButton'
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 type formularioProp = StackNavigationProp<RootStackParamList, 'Formulario'>;
+moment().format();
 
 export default function Formulario() {
   const [idade, setIdade] = useState('');
-  const [dataNascimento, setDataNascimento] = useState(new Date());
-  const [open, setOpen] = useState(false)
 
   const navigation = useNavigation<formularioProp>();
 
@@ -23,69 +21,25 @@ export default function Formulario() {
   }
 
   function calcularIdade(dataNascimento: string): void{
+    if(dataNascimento.length !== 10){
+      return;
+    }
+
     const idade = moment(new Date()).diff(dataNascimento, 'years');
-    setIdade(idade.toString());
-  }
-/*
-  function completarDataNascimento(value: string): void{
-    console.log(value)
-    if(value.length === 2){
-      /*const ultimoCaractere = obterUltimoCaractere(value);
-
-      if(ultimoCaractere !== "/"){
-        setDataNascimento(value)
-        return;
-      }
-
-
-      value += "/";
-    }
-
-    if(value.length === 5){
-      const ultimoCaractere = obterUltimoCaractere(value);
-      console.log(ultimoCaractere)
-
-      if(ultimoCaractere !== "/"){
-        setDataNascimento(value)
-        return;
-      }
-      value += "/";
-    }
-
-    setDataNascimento(value)
-  }*/
-
-  //https://github.com/react-native-datetimepicker/datetimepicker
-
-  function obterUltimoCaractere(value: string): string{
-    return value.slice(-1)
+    console.log(idade);
+    setIdade(idade.toString() ?? "");
   }
 
   return (
     <>
-<Button title="Open" onPress={() => setOpen(true)} />
-      <DatePicker
-        modal
-        open={open}
-        date={dataNascimento}
-        onConfirm={(date) => {
-          setOpen(false)
-          setDataNascimento(date)
-        }}
-        onCancel={() => {
-          setOpen(false)
-        }}
-      />
-
       <View style={styles.container}>
         <SafeAreaView style={{flex: 1}}>
           <Text style={styles.titulo}>Por favor, entre com os dados abaixo:</Text>
 
           <TextInput style={styles.input} placeholder='Nome do paciente' />
 
-          <TextInput keyboardType='numeric' style={styles.input} 
+          <TextInput keyboardType='numbers-and-punctuation' style={styles.input} 
             placeholder='Data de nascimento (dd/mm/aaaa)' 
-            value={dataNascimento.toString()}
             onChangeText={value => calcularIdade(value)} 
           />
 
@@ -127,20 +81,3 @@ const styles = StyleSheet.create({
     padding: '2%',
   },
 });
-
-/*<View style={globalStyles.container}>
-            <Text style={globalStyles.titulo}>Por favor, entre com os dados abaixo:</Text>
-
-            <TextInput style={styles.input} placeholder='Nome do paciente' />
-
-            <TextInput keyboardType='numbers-and-punctuation' style={[styles.input]} placeholder='Data de nascimento (yyyy-mm-dd)' 
-                  onChangeText={value => calcularIdade(value)} />
-
-            <TextInput style={styles.input} placeholder='Idade' editable={false} value={idade} />
-
-            <TextInput style={[styles.input, { marginTop: 40 }]} placeholder='Nome do acompanhante' />
-
-            <TextInput keyboardType='phone-pad' style={styles.input} placeholder='Contato do acompanhante' />
-
-            <RoundedButton title='ENTRAR' onPress={conectar} />
-        </View>*/
